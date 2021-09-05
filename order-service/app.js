aa603514d913a7ee4ba7c36344b2de786b785649
Rafi <t-rafi.abiyyi@traveloka.com>
2021-09-05 17:35:51 +0700

    base [tmp] order service @ order

---
+++ order-service/app.js
@@ -0,0 +1,78 @@
+const express = require("express")
+const app = express()
+
+const PORT = process.env.PORT_ONE || 3030
+
+const mongoose = require("mongoose")
+const jwt = require("jsonwebtoken")
+
+mongoose.connect(
+    "mongodb://localhost/dineinn-order-service",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    () => {
+        console.log(`Product-Service DB Connected`)
+    }
+)
+
+app.use(express.json())
+
+// crud product tmp
+const Order = require('./OrderSchema')
+const isAuthenticated = require('../utils/middleware/isAuthenticated')
+
+const createOrder = (products, userEmail) => {
+    let total = 0
+
+    for (let i=0; i<products.length; i++) {
+        total += products[i].price
+    }
+
+    console.log(products)
+
+    const newOrder = new Order({
+        products,
+        user: userEmail,
+        total_price: total
+    })
+    newOrder.save()
+
+    return newOrder
+}
+
+// rabbit mq
+// docker run -p 5672:5672 -p 15672:15672 rabbitmq:management
+var channel, amqpConnection
+
+const amqp = require('amqplib')
+const amqp_connect = async () => {
+
+    const amqpServer = 'amqp://localhost:5672'
+
+    amqpConnection = await amqp.connect(amqpServer)
+    console.log('connected')
+
+    channel = await amqpConnection.createChannel()
+    console.log('create channel')
+
+    await channel.assertQueue('ORDER')
+}
+
+// TODO: add RPC
+amqp_connect().then( () => {
+    channel.consume('ORDER', (data) => {
+        const { products, userEmail } = JSON.parse(data.content)
+        console.log('consuming ORDER queue')
+        console.log(products)
+
+        const newOrder = createOrder(products, userEmail)
+
+        channel.ack(data)
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Order-Service at ${PORT}`)
+});
